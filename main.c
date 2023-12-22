3746a4cee09767cb86558b7bdb6689e77ba62e4c
zmladen <zmladen@gmail.com>
2023-12-22 11:04:50 +0100

    main.c added calculation of CPU time
    User name changed
    
    :q

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>  // Include time.h for clock()
 #include "utility_functions.h"
 #include <Python.h>
 
@@ -6,6 +7,7 @@ int main() {
     Py_Initialize();
     // PyRun_SimpleString("import sys");
     // PyRun_SimpleString("sys.path.append('./venv/Lib/site-packages')");
+
     // Get and print PyTorch version
     const char* torch_version = get_pytorch_version();
     if (torch_version != NULL) {
@@ -19,11 +21,22 @@ int main() {
     double numbers[] = {11.0, 2.5, 3.2, 4.8};
     int size = sizeof(numbers) / sizeof(numbers[0]);
 
-    // Call the function to calculate the mean and print the result
+    // Start timing
+    clock_t start = clock();
+
+    // Call the function to calculate the mean
     double result = call_python_function(numbers, size);
+
+    // End timing
+    clock_t end = clock();
+
+    // Calculate the time taken in seconds
+    double time_taken = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+    // Print the result and time taken
     printf("Result of call: %f\n", result);
+    printf("Time taken for Python function: %f seconds\n", time_taken);
 
     Py_Finalize();
     return 0;
 }
-
